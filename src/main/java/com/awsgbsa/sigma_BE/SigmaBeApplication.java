920559265780b5e7bdd011ee2f591b4727eb94d8
rudalsss <linda284@naver.com>
2025-08-10 01:33:32 +0900

    Initial commit

---
+++ src/main/java/com/awsgbsa/sigma_BE/SigmaBeApplication.java
@@ -0,0 +1,13 @@
+package com.awsgbsa.sigma_BE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SigmaBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SigmaBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/awsgbsa/sigma_BE/SigmaBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.awsgbsa.sigma_BE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SigmaBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
