7360bcb76c2a5be25dd43cd8e2848bd81f714277
rudalsss <linda284@naver.com>
2025-08-16 23:06:42 +0900

    [feat] eks설정 추가 및 변경

--- src/main/java/com/awsgbsa/sigma_BE/user/controller/AuthController.java
+++ src/main/java/com/awsgbsa/sigma_BE/user/controller/AuthController.java
@@ -12,7 +12,6 @@ import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
